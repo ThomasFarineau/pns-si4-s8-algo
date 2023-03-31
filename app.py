1cb172c8d5250c83731fd906b6481c2a273c2c45
Thomas <thomas.farineau@icloud.com>
2023-03-31 11:38:58 +0200

    ajout de message spécifique a l'erreur de format

--- app.py
+++ app.py
@@ -9,23 +9,33 @@ def move():
     b = request.args.get('b')
     if b is None:
         return send_detail('Paramètre b manquant', 400)
-    if not is_valid(b):
-        return send_detail('Paramètre b invalide', 400)
+    check = check_format(b)
+    if not check[0]:
+        return send_detail(check[1], 400)
     # TODO: effectuer les opérations nécessaires avec le paramètre b
     print_board(parse_board(b))
     result = {'message': 'Opération effectuée avec succès'}
     return jsonify(result)
 
 
-def is_valid(b):
+def check_format(b):
     # check length is 42
     if len(b) != 42:
-        return False
+        return False, "La chaîne de caractères doit contenir 42 caractères"
     # check if only 0, m or h
     for c in b:
         if c not in ['0', 'm', 'h']:
-            return False
-    return True
+            return False, "La chaîne de caractères ne doit contenir que des 0, des m ou des h"
+    for i in range(7):
+        for j in range(6):
+            index = i * 6 + j
+            # check if there is a 0 before an m or h
+            if b[index] != '0' and b[index - 1] == '0' and index % 6 != 0:
+                return False, "Il ne peut pas y avoir de 0 avant un m ou un h dans la même colonne"
+    # check if h = m + 1
+    if b.count('h') != b.count('m') + 1:
+        return False, "Il doit y avoir n(h) = n(m) + 1"
+    return True, "Oui"
 
 
 def send_detail(detail, status):
