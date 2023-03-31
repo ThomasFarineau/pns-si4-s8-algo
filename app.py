b9c0f7acdfe1cba4f5688247b5547909d65e252a
LéoK <leo.kitabdjian06@gmail.com>
2023-03-31 11:41:11 +0200

    début fonction score

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from algo import *
 
 app = Flask(__name__)
 port = 5000
@@ -13,8 +14,10 @@ def move():
     if not check[0]:
         return send_detail(check[1], 400)
     # TODO: effectuer les opérations nécessaires avec le paramètre b
-    print_board(parse_board(b))
+    board = parse_board(b)
+    print(board)
     result = {'message': 'Opération effectuée avec succès'}
+    print(score(board))
     return jsonify(result)
 
 
--- main.py
+++
@@ -1,3 +0,0 @@
-def score();
-    # do some stuff
-    return score
