c72d17bbddb03cf5869edd27aad78c84fc2f797b
TomBevan <bevantom@protonmail.com>
2023-04-07 10:04:38 +0200

    Auto stash before merge of "master" and "origin/master"

--- algo.py
+++ algo.py
@@ -1,3 +1,5 @@
+import math
+import random
 score_heuristics = [[2, 2, 2, 2, 2, 2], [3, 4, 5, 5, 4, 3], [4, 6, 8, 8, 6, 4], [6, 8, 10, 10, 8, 6], [4, 6, 8, 8, 6, 4], [3, 4, 5, 5, 4, 3], [2, 2, 2, 2, 2, 2]]
 
 def score(board):
@@ -11,3 +13,24 @@ def score(board):
                 #print("-", score_heuristics[i][j])
                 score -= score_heuristics[i][j]
     return score
+
+
+
+
+def next_move(last_move):
+    #time = perfomance.now();
+    play(grid, last_move[0], last_move[1], currentPlayer)
+    if (last_move.length != 0):
+        if (currentPlayer == 'm'):
+            currentPlayer = 'h'
+        elif (currentPlayer == 'h'):
+            currentPlayer = 'm'
+    ret
+    finalRet
+    depth = 1
+    while(depth <= 42):  # time should be checked
+        finalRet = ret
+        ret = minimax(grid, currentPlayer, -100000, 100000, currentPlayer)[1]
+        depth += 1
+    if (finalRet[0] == -1 and finalRet[1] == -1):
+        finalRet = playableMoves(grid)[random.randint(0, 7)]
